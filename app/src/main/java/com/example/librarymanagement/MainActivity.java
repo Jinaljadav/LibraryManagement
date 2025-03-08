f020b25a94f0f60f0bf04d0c90f747742b8cf633
Jinaljadav <86101125+Jinaljadav@users.noreply.github.com>
2025-03-08 15:13:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/librarymanagement/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.librarymanagement;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        //Define a textview
+        TextView textView = findViewById(R.id.textView);
+
+        //Define a buttons
+        Button borrow = findViewById(R.id.BorrowBook);
+
+        //Function to the button
+
+        borrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Naviagte to borrow a book page
+
+                Intent intent= new Intent(MainActivity.this,BorrowActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
